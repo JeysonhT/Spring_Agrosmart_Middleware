5540047d78a574347dc0fc6cc5bb62ab2ae04f20
Jeysonh <jasontellezaguilar@gmail.com>
2025-09-30 21:44:48 -0600

    end point de health check añadido

--- src/main/java/com/agrosmart/middleware/controllers/RecommendationController.java
+++ src/main/java/com/agrosmart/middleware/controllers/RecommendationController.java
@@ -6,6 +6,7 @@ import com.agrosmart.middleware.services.interfaces.IGeminiRequestService;
 import java.util.concurrent.CompletableFuture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,18 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/recommendation")
 public class RecommendationController {
-    
+
     @Autowired
     private IGeminiRequestService grs;
-    
+
     @PostMapping("/getRecommendation")
-    public CompletableFuture<ResponseEntity<Response>> getRecommendation(@RequestBody Request request){
+    public CompletableFuture<ResponseEntity<Response>> getRecommendation(@RequestBody Request request) {
         return grs.getPromptResponse(request)
                 .thenApply(response -> {
-                    if(response.getResponse().startsWith("Error")){
+                    if (response.getResponse().startsWith("Error")) {
                         return ResponseEntity.internalServerError().body(response);
                     }
                     return ResponseEntity.ok(response);
                 });
+    }
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK";
     }
 }
